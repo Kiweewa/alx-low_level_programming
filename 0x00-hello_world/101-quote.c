fdf94d34ee45f80a99654462335394f324fde282
Kiweewa <you@example.com>
2023-02-16 17:29:20 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	write(2, "and that piece of art is usefull\" -Dora Koper, 2015-10-19\n", 59);
+	return (1);
+}
