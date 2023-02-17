2af88b15054999787d5f767cbe610a85dfe21c9a
Kiweewa <you@example.com>
2023-02-16 16:47:55 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio,h>
 /**
- * main - print out a string to stdout
+ * main - print the code using printf
  *
  * Return: 0 if success
  */
 int main(void)
 {
 	Printf("which proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
