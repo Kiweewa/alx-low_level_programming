9f92cf333745d4a90f975174240e53de30e43612
Kiweewa <you@example.com>
2023-02-17 17:45:50 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,12 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else if (n > 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	} else
 	{
-		printf("invalid input");
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
