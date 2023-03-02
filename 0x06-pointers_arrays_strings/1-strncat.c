108243b2ada4da04d0e7c1e170e459c3f4981d0c
Kiweewa <you@example.com>
2023-03-02 13:12:05 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,14 @@
 
 /**
  * _strncat - concatenates two strings
- *  using at most n bytes from src
+ * using at most n bytes from src
  * @dest: input value
  * @src: input value
  * @n: input value
  *
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 	int x;
 	int v;
